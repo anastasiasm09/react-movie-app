d8b778bfdaa7cdc74713ee6759caf42daada5c8f
Anastasiia <anastasiia@MacBook-Pro-13.local>
2025-04-21 22:28:37 +0100

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { useQuery } from "@tanstack/react-query";
 import { Link, Routes, Route } from "react-router-dom";
 import MovieList from "./components/MovieList";
 import Movie from "./components/Movie";
-
+import Footer from "./components/Footer";
 function App() {
-  const title = "MOVIES";
+  const title = "RM.app";
 
   return (
     <HeroUIProvider>
@@ -15,6 +15,7 @@ function App() {
           <Route path="/" element={<MovieList />} />
           <Route path="/movies/:id" element={<Movie />} />
         </Routes>
+        <Footer />
       </main>
     </HeroUIProvider>
   )
