fb810cf02193f6d7d4c7308635459c067c1e52c0
Anastasiia <anastasiia@MacBook-Pro-13.local>
2025-04-23 17:04:26 +0100

    added env

--- src/requests/utils.js
+++ src/requests/utils.js
@@ -3,6 +3,6 @@ export const requestOptions = {
     headers: {
         accept: 'application/json',
         'Content-Type': 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNjQxY2Y1NGI3ZDlhZTI2NjQ0YTQ5YWI1YzMxYmFhMyIsIm5iZiI6MTc0MjU1NjQ4OS43NTUsInN1YiI6IjY3ZGQ0ZDQ5MDQxNjg3NWFkYzY5ODNlMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4BHTpi8ZBwBsQFQ9wSZ17es4_C6OHCQMf7dTmwWHv8o'
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_BEARER_TOKEN}`
     }
 };
